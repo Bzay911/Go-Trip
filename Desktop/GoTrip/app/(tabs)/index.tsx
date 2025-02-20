adc0b8c3bb19d2da3550500098375ef8aea22c4b
bijayagurung <gurungbeejaya@gmail.com>
2025-02-21 09:10:14 +1100

    Updated index page

--- Desktop/GoTrip/app/(tabs)/index.tsx
+++ Desktop/GoTrip/app/(tabs)/index.tsx
@@ -8,6 +8,7 @@ const index = () => {
 
   return (
     <SafeAreaView style={{flex:1}}>
+      <Text style={styles.mainTitle}>Travelling Today ?</Text>
        <Text style={styles.title}>Perfect Hangouts For {contextWeather} Weather</Text>
      <RecommendedPlacesList places={contextPlaces} location={contextLocation} />
     </SafeAreaView>
@@ -17,6 +18,12 @@ const index = () => {
 export default index
 
 const styles = StyleSheet.create({
+  mainTitle: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    textAlign:'left',
+    margin: 16},
+    
   title: {
     fontWeight: 'bold',
     fontSize: 18,
