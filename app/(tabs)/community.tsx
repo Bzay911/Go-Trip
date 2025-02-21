9334bff9c821837244993b466987e319ea53235d
bijayagurung <gurungbeejaya@gmail.com>
2025-02-21 12:34:32 +1100

    Initial commit in correct directory

---
+++ app/(tabs)/community.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const communityScreen = () => {
+  return (
+    <View>
+      <Text>communityScreen</Text>
+    </View>
+  )
+}
+
+export default communityScreen
