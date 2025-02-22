21c359a8a758fd35e885f671f9ee9e794013a777
Sumit <sumit@192-168-1-11.tpgi.com.au>
2025-02-23 08:44:15 +1100

    recent changes

--- .expo/types/router.d.ts
+++ .expo/types/router.d.ts
@@ -6,9 +6,9 @@ export * from 'expo-router';
 declare module 'expo-router' {
   export namespace ExpoRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/community` | `/(tabs)/dynamicPlanner` | `/(tabs)/profile` | `/(tabs)/search` | `/_sitemap` | `/community` | `/dynamicPlanner` | `/placeDetailsScreen` | `/placesContext` | `/profile` | `/recommendedPlacesList` | `/search`;
-      DynamicRoutes: never;
-      DynamicRouteTemplate: never;
+      StaticRoutes: `/` | `/(tabs)` | `/(tabs)/` | `/(tabs)/communityPage` | `/(tabs)/dynamicPlanner` | `/(tabs)/profile` | `/(tabs)/search` | `/_sitemap` | `/communityPage` | `/dynamicPlanner` | `/placeDetailsScreen` | `/placesContext` | `/profile` | `/recommendedPlacesList` | `/search` | `/uploadScreen`;
+      DynamicRoutes: `/addComment/${Router.SingleRoutePart<T>}`;
+      DynamicRouteTemplate: `/addComment/[postId]`;
     }
   }
 }
---
+++ Config/config.tsx
@@ -0,0 +1,9 @@
+export  const firebaseConfig = {
+    apiKey: "AIzaSyANmmA1LfF4R_-YxyQQmm_Uqyj3XlLhjHc",
+    authDomain: "go-trip-7400b.firebaseapp.com",
+    projectId: "go-trip-7400b",
+    storageBucket: "go-trip-7400b.firebasestorage.app",
+    messagingSenderId: "426751250539",
+    appId: "1:426751250539:web:903eca49462981fb2ea12e",
+    measurementId: "G-WGGCDKYCXX"
+  };
